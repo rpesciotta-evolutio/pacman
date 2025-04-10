8afc24cdc9843954ad8834afdbeb5a31bb06eee7
Ricardo Pesciotta <ricardo.pesciotta@evolutiops.com>
2025-04-10 08:59:38 +0000

    bumps mongo driver version to support reconnection to db adds manual instrumentation to db actions to report attribute db.connection_error:true adds manual instrumentation to all routes that access the db to add attribute requires_db:true for statistics fixes highscores/list route to not attempt to process result when db connection fails

--- routes/highscores.js
+++ routes/highscores.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var Database = require('../lib/database');
+const { trace, context, SpanKind } = require('@opentelemetry/api');
+const tracer = trace.getTracer('highscores-route');
 
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -14,26 +16,41 @@ router.use(function timeLog (req, res, next) {
 
 router.get('/list', urlencodedParser, function(req, res, next) {
     console.log('[GET /highscores/list]');
+
+    const currentSpan = trace.getSpan(context.active());
+    if (currentSpan) currentSpan.setAttribute('requires_db', true);
+
     Database.getDb(req.app, function(err, db) {
         if (err) {
-            return next(err);
+            return next(err); // let Express handle other types of errors
         }
 
-        // Retrieve the top 10 high scores
-        var col = db.collection('highscore');
-        col.find({}).sort([['score', -1]]).limit(10).toArray(function(err, docs) {
-            var result = [];
-            if (err) {
-                console.log(err);
-            }
+        const findSpan = tracer.startSpan('MongoDB find highscore', { kind: SpanKind.SERVER }, context.active());
+        context.with(trace.setSpan(context.active(), findSpan), () => {
+            // Retrieve the top 10 high scores wrapped in an OpenTelemetry span
+            var col = db.collection('highscore');            
+            col.find({}).sort([['score', -1]]).limit(10).toArray(function(err, docs) {
+                if (err) {
+                    findSpan.recordException(err);
+                    findSpan.setStatus({ code: 2, message: 'Failed communication with the DB' });
+                    findSpan.setAttribute('db.connection_error', true);
+                    findSpan.end();
+                    console.log('Error retrieving high scores:', err);
+                    console.error(err);
+                    return next(err); // let Express handle other types of errors
+                }
 
-            docs.forEach(function(item, index, array) {
-                result.push({ name: item['name'], cloud: item['cloud'],
-                              zone: item['zone'], host: item['host'],
-                              score: item['score'] });
+                findSpan.setStatus({ code: 1, message: 'Success' });
+                findSpan.end();
+
+                var result = [];
+                docs.forEach(function(item, index, array) {
+                    result.push({ name: item['name'], cloud: item['cloud'],
+                                zone: item['zone'], host: item['host'],
+                                score: item['score'] });
+                });
+                res.json(result);
             });
-
-            res.json(result);
         });
     });
 });
@@ -48,13 +65,15 @@ router.post('/', urlencodedParser, function(req, res, next) {
     var userScore = parseInt(req.body.score, 10),
         userLevel = parseInt(req.body.level, 10);
 
-    Database.getDb(req.app, function(err, db) {
-        if (err) {
-            return next(err);
-        }
+    const currentSpan = trace.getSpan(context.active());
+    if (currentSpan) currentSpan.setAttribute('requires_db', true);
 
-        // Insert high score with extra user data
-        db.collection('highscore').insertOne({
+    Database.getDb(req.app, function(err, db) {
+        if (err) return next(err);
+
+        const insertSpan = tracer.startSpan('MongoDB insert highscore', { kind: SpanKind.SERVER }, context.active());
+        context.with(trace.setSpan(context.active(), insertSpan), () => {
+            db.collection('highscore').insertOne({
                 name: req.body.name,
                 cloud: req.body.cloud,
                 zone: req.body.zone,
@@ -72,15 +91,19 @@ router.post('/', urlencodedParser, function(req, res, next) {
                 wtimeout: 10000
             }, function(err, result) {
                 var returnStatus = '';
-
                 if (err) {
-                    console.log(err);
+                    insertSpan.recordException(err);
+                    insertSpan.setStatus({ code: 2, message: 'Failed communication with the DB' });
+                    insertSpan.setAttribute("db.connection_error", true);
+                    console.log('Error inserting high score:', err);
+                    console.error(err);
                     returnStatus = 'error';
                 } else {
                     console.log('Successfully inserted highscore');
+                    insertSpan.setStatus({ code: 1, message: 'Success' });
                     returnStatus = 'success';
                 }
-
+                insertSpan.end();
                 res.json({
                     name: req.body.name,
                     zone: req.body.zone,
@@ -89,6 +112,7 @@ router.post('/', urlencodedParser, function(req, res, next) {
                     rs: returnStatus
                 });
             });
+        });
     });
 });
 
--- routes/user.js
+++ routes/user.js
@@ -3,10 +3,11 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var ObjectId = require('mongodb').ObjectId;
 var Database = require('../lib/database');
+const { trace, context, SpanKind } = require('@opentelemetry/api');
+const tracer = trace.getTracer('user-route');
 
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
 
 // middleware that is specific to this router
 router.use(function timeLog (req, res, next) {
@@ -16,31 +17,41 @@ router.use(function timeLog (req, res, next) {
 
 router.get('/id', function(req, res, next) {
     console.log('[GET /user/id]');
+    const currentSpan = trace.getSpan(context.active());
+    if (currentSpan) currentSpan.setAttribute('requires_db', true);
+
     Database.getDb(req.app, function(err, db) {
-        if (err) {
-            return next(err);
-        }
+        if (err) return next(err);
 
         // Insert user ID and return back generated ObjectId
         var userId = 0;
-        db.collection('userstats').insertOne({
-            date: Date()
-        }, {
-           w: 'majority',
-           j: true,
-           wtimeout: 10000
-        }, function(err, result) {
-           if (err) {
-               console.log('failed to insert new user ID err =', err);
-           } else {
-               userId = result.insertedId;
-               console.log('Successfully inserted new user ID = ', userId);
-           }
 
-           res.json(userId);
+        const insertSpan = tracer.startSpan('MongoDB insert user id', { kind: SpanKind.SERVER }, context.active());
+        context.with(trace.setSpan(context.active(), insertSpan), () => {
+            db.collection('userstats').insertOne({
+                date: Date()
+            }, {
+                w: 'majority',
+                j: true,
+                wtimeout: 10000
+            }, function(err, result) {
+                if (err) {
+                    console.log('failed to insert new user ID err =', err);
+                    console.error(err);
+                    insertSpan.recordException(err);
+                    insertSpan.setStatus({ code: 2, message: 'Failed to insert new user ID' });
+                    insertSpan.setAttribute('db.connection_error', true);
+                } else {
+                    insertSpan.setStatus({ code: 1, message: 'Success' });
+                    userId = result.insertedId;
+                    console.log('Successfully inserted new user ID = ', userId);
+                }
+
+                insertSpan.end();
+                res.json(userId);
+            });
         });
     });
-
 });
 
 router.post('/stats', urlencodedParser, function(req, res, next) {
@@ -55,48 +66,58 @@ router.post('/stats', urlencodedParser, function(req, res, next) {
         userLives = parseInt(req.body.lives, 10),
         userET = parseInt(req.body.elapsedTime, 10);
 
+    const currentSpan = trace.getSpan(context.active());
+    if (currentSpan) currentSpan.setAttribute('requires_db', true);
+
     Database.getDb(req.app, function(err, db) {
-        if (err) {
-            return next(err);
-        }
+        if (err) return next(err);
 
-        // Update live user stats
-        db.collection('userstats').updateOne({
-                _id: new ObjectId(req.body.userId),
-            }, { $set: {
-                    cloud: req.body.cloud,
-                    zone: req.body.zone,
-                    host: req.body.host,
-                    score: userScore,
-                    level: userLevel,
-                    lives: userLives,
-                    elapsedTime: userET,
-                    date: Date(),
-                    referer: req.headers.referer,
-                    user_agent: req.headers['user-agent'],
-                    hostname: req.hostname,
-                    ip_addr: req.ip
-               }, $inc: {
-                    updateCounter: 1
-               }
-            }, {
-                w: 'majority',
-                j: true,
-                wtimeout: 10000
-            }, function(err, result) {
-                var returnStatus = '';
+        const updateSpan = tracer.startSpan('MongoDB update user stats', { kind: SpanKind.SERVER }, context.active());
+        context.with(trace.setSpan(context.active(), updateSpan), () => {
+            // Update live user stats
+            db.collection('userstats').updateOne({
+                    _id: new ObjectId(req.body.userId),
+                }, { $set: {
+                        cloud: req.body.cloud,
+                        zone: req.body.zone,
+                        host: req.body.host,
+                        score: userScore,
+                        level: userLevel,
+                        lives: userLives,
+                        elapsedTime: userET,
+                        date: Date(),
+                        referer: req.headers.referer,
+                        user_agent: req.headers['user-agent'],
+                        hostname: req.hostname,
+                        ip_addr: req.ip
+                    }, $inc: {
+                        updateCounter: 1
+                    }
+                }, {
+                    w: 'majority',
+                    j: true,
+                    wtimeout: 10000
+                }, function(err, result) {
+                    var returnStatus = '';
 
-                if (err) {
-                    console.log(err);
-                    returnStatus = 'error';
-                } else {
-                    console.log('Successfully updated user stats');
-                    returnStatus = 'success';
-                }
+                    if (err) {
+                        console.log('failed to update user stats err =', err);
+                        console.error(err);
+                        updateSpan.recordException(err);
+                        updateSpan.setStatus({ code: 2, message: 'Failed to update user stats' });
+                        updateSpan.setAttribute('db.connection_error', true);
+                        returnStatus = 'error';
+                    } else {
+                        console.log('Successfully updated user stats');
+                        updateSpan.setStatus({ code: 1, message: 'Success' });
+                        returnStatus = 'success';
+                    }
 
-                res.json({
-                    rs: returnStatus
-                });
+                    updateSpan.end();
+                    res.json({
+                        rs: returnStatus
+                    });
+            });
         });
     });
 });
@@ -104,34 +125,47 @@ router.post('/stats', urlencodedParser, function(req, res, next) {
 router.get('/stats', function(req, res, next) {
     console.log('[GET /user/stats]');
 
+    const currentSpan = trace.getSpan(context.active());
+    if (currentSpan) currentSpan.setAttribute('requires_db', true);
+
     Database.getDb(req.app, function(err, db) {
-        if (err) {
-            return next(err);
-        }
+        if (err) return next(err);
 
-        // Find all elements where the score field exists to avoid
-        // undefined values
-        var col = db.collection('userstats');
-        col.find({ score: {$exists: true}}).sort([['_id', 1]]).toArray(function(err, docs) {
-            var result = [];
-            if (err) {
-                console.log(err);
-            }
+        const findSpan = tracer.startSpan('MongoDB find user stats', { kind: SpanKind.SERVER }, context.active());
+        context.with(trace.setSpan(context.active(), findSpan), () => {
 
-            docs.forEach(function(item, index, array) {
-                result.push({
-                                cloud: item['cloud'],
-                                zone: item['zone'],
-                                host: item['host'],
-                                score: item['score'],
-                                level: item['level'],
-                                lives: item['lives'],
-                                et: item['elapsedTime'],
-                                txncount: item['updateCounter']
+            // Find all elements where the score field exists to avoid
+            // undefined values
+            var col = db.collection('userstats');
+            col.find({ score: {$exists: true}}).sort([['_id', 1]]).toArray(function(err, docs) {
+                var result = [];
+                if (err) {
+                    console.log('failed to find user stats err =', err);
+                    console.error(err);
+                    findSpan.recordException(err);
+                    findSpan.setStatus({ code: 2, message: 'Failed to find user stats' });
+                    findSpan.setAttribute('db.connection_error', true);
+                    findSpan.end();
+                    return next(err);
+                }
+
+                findSpan.setStatus({ code: 1, message: 'Success' });
+                findSpan.end();
+                docs.forEach(function(item, index, array) {
+                    result.push({
+                                    cloud: item['cloud'],
+                                    zone: item['zone'],
+                                    host: item['host'],
+                                    score: item['score'],
+                                    level: item['level'],
+                                    lives: item['lives'],
+                                    et: item['elapsedTime'],
+                                    txncount: item['updateCounter']
+                    });
                 });
-            });
 
-            res.json(result);
+                res.json(result);
+            });
         });
     });
 });
